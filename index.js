0cba97ad473da59b6f38c26fced4d28bdb7ca206
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-09-11 17:06:58 -0700

    updating .catch

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-// Add your code here
-//fetch(destinationURL, configurationObject);
-// const destinationURL = "http://localhost:3000/dogs"
-
-
-// const configurationObject = {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json",
-//         "Accept": "application/json"
-//     },
-//     body: JSON.stringify({
-//         dogName: "Byron",
-//         dogBreed : "Poodle"
-//     })
-// };
-// fetch("http://localhost:3000/dogs", configurationObject)
-// .then(function (response){
-//     return response.json();
-// })
-// .then(function(object){
-//     console.log(object)
-// })
-
-// const users = {
-//     userName : "Lana",
-//     email: "svetlana.kisel.ca@gmail.com"
-// }
-function configurationObject (userName, email) {
-return { method : 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept' : 'application/json'
-    },
-    body: JSON.stringify({
-        'name':userName, 
-        'email': email
-    })
-        }    
+function configurationObject(userName, email) {
+    return {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': userName,
+            'email': email
+        })
     }
-function submitData(userName, email){
-    //fetch('http://localhost:3000/users', configurationObject)
-    //.then(response => {response.json().then(parsed => {console.log(parsed)})})
-    // .catch(function(error){
-    //     alert('Oh man..It didn\'t work');
-    //     console.log(error.message)})
-        return fetch("http://localhost:3000/users", configurationObject(userName, email))
+}
+function submitData(userName, email) {
+    return fetch("http://localhost:3000/users", configurationObject(userName, email))
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            document.querySelector("body").innerHTML = data.id;
+            let p = document.createElement('p')
+            p.textContent = data.id
+            document.querySelector("body").appendChild(p)
         })
         .catch((error) => {
-          document.querySelector("body").innerHTML = error.message;
+            document.querySelector("body").innerHTML = error.message;
         });
-    }
-    
+}
